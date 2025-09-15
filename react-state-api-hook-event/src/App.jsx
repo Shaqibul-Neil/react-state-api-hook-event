feba47abd7308664aa56884ead2c5f66de2204b0
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-09-15 23:33:08 +0600

    event

--- react-state-api-hook-event/src/App.jsx
+++ react-state-api-hook-event/src/App.jsx
@@ -1,9 +1,25 @@
-import { useState } from "react";
-
-import "./App.css";
+import './App.css';
+import Button from './Button';
 
 function App() {
-  return;
+  const handleClick = () => alert("I'm clicked");
+
+  const handleAdd5 = num => {
+    const newNum = num + 5;
+    alert(newNum);
+  };
+  const multiply = (a, b) => alert(a * b);
+
+  return (
+    <>
+      <Button onClick={handleClick} />
+      <Button />
+      {/* parameter sending in function */}
+      {/* wrap it up in another arrow function */}
+      <button onClick={() => handleAdd5(7)}>Click Me Num</button>
+      <button onClick={() => multiply(5, 7)}>Click to multiply</button>
+    </>
+  );
 }
 
 export default App;
---
+++ react-state-api-hook-event/src/Button.jsx
@@ -0,0 +1,5 @@
+const Button = ({ onClick }) => {
+  //sending the function through props as obj
+  return <button onClick={onClick}>Click Me</button>;
+};
+export default Button;
