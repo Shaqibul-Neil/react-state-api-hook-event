da042a23fd96d220b5aa53abafcf36e6341fbf90
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-09-16 11:43:25 +0600

    check

--- react-state-api-hook-event/src/App.jsx
+++ react-state-api-hook-event/src/App.jsx
@@ -6,7 +6,7 @@ import Users from './Users';
 import Friends from './Friends';
 import Posts from './Posts';
 import { Suspense } from 'react';
-
+//neil
 const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users').then(
   response => response.json()
 );
